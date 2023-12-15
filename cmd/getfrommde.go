b99727f3d579cc1c7cae912176931794fbef3be4
Olaf Hartong <8149899+olafhartong@users.noreply.github.com>
2023-12-15 21:53:49 +0100

    adding user-agents to requests

--- cmd/getfrommde.go
+++ cmd/getfrommde.go
@@ -32,6 +32,7 @@ func GetDataFromMDE(accessToken string, endpoint string, queryParams string, sen
 
 	req.Header.Set("Authorization", "Bearer "+accessToken)
 	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0 OS/10.0.22621")
 
 	client := &http.Client{
 		Timeout: 10 * time.Second,
